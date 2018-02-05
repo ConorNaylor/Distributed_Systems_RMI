1af9dcbb1eb549e998c31ffe00a9eab17c116f1e
Bronagh <bronaghdeane96@gmail.com>
2018-02-05 17:52:21 +0000

    implementing exam engine methods

--- src/server/ExamEngine.java
+++ src/server/ExamEngine.java
@@ -1,10 +1,10 @@
-
 package server;
 
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
+import java.util.ArrayList;
 import java.util.List;
 
 import exceptions.NoMatchingAssessment;
@@ -12,66 +12,120 @@ import exceptions.UnauthorizedAccess;
 import interfaces.Assessment;
 import interfaces.ExamServer;
 
-public class ExamEngine implements ExamServer {
+public class ExamEngine extends UnicastRemoteObject implements ExamServer {
 
-    // Constructor is required
-    public ExamEngine() {
-        super();
-    }
+	private ArrayList<Student> students;
+	private ArrayList<SessionToken> sessions;
+	private ArrayList<Assessment> assessments;
 
-    // Implement the methods defined in the ExamServer interface...
-    // Return an access token that allows access to the server for some time period
-    public int login(int studentid, String password) throws 
-                UnauthorizedAccess, RemoteException {
+	// Constructor is required
+	public ExamEngine() throws RemoteException {
+		super();
+	}
 
-	// TBD: You need to implement this method!
-	// For the moment method just returns an empty or null value to allow it to compile
+	// Implement the methods defined in the ExamServer interface...
+	// Return an access token that allows access to the server for some time period
+	public int login(int studentid, String password) throws UnauthorizedAccess, RemoteException {
+		SessionToken token = null;
+		for (Student student : students) {
+			if (studentid == (student.getstudentID())) {
+				if (password.equals(student.getPassword())) {
+					token = new SessionToken(student);
+					sessions.add(token);
 
-	return 0;	
-    }
+				} else {
+					throw new UnauthorizedAccess("Incorrect password");
+				}
+			} else {
+				throw new UnauthorizedAccess("Student not registered");
+			}
+		}
+		return token.getToken();
+	}
 
-    // Return a summary list of Assessments currently available for this studentid
-    public List<String> getAvailableSummary(int token, int studentid) throws
-                UnauthorizedAccess, NoMatchingAssessment, RemoteException {
+	// Return a summary list of Assessments currently available for this studentid
+	public List<String> getAvailableSummary(int token, int studentid)
+			throws UnauthorizedAccess, NoMatchingAssessment, RemoteException {
+		List<String> summary = new ArrayList<String>();
+		for (SessionToken session : sessions) {
+			if (session.getToken() == token && session.getStudent().getstudentID() == studentid) {
+				if (assessments.isEmpty()) {
+					for (Assessment assessment : assessments) {
+						summary.add(assessment.getInformation());
+					}
+				} else {
+					throw new NoMatchingAssessment("No assessments available");
+				}
+			} else {
+				throw new UnauthorizedAccess("Session expired");
+			}
+		}
+		return summary;
+	}
 
-        // TBD: You need to implement this method!
-        // For the moment method just returns an empty or null value to allow it to compile
+	// Return an Assessment object associated with a particular course code
+	public Assessment getAssessment(int token, int studentid, String courseCode)
+			throws UnauthorizedAccess, NoMatchingAssessment, RemoteException {
+		if (!sessions.isEmpty()) {
+			for (SessionToken session : sessions) {
+				if (session.getToken() == token && session.getStudent().getstudentID() == studentid) {
+					if (assessments.isEmpty()) {
+						for (Assessment assessment : assessments) {
+							if (assessment.getAssociatedID() == studentid
+							// && assessment.getCourseCode().equals(courseCode)
+							) {
+								return assessment;
+							}
+						}
+					} else {
+						throw new NoMatchingAssessment("No assessments available");
+					}
+				} else {
+					throw new UnauthorizedAccess("Session expired");
+				}
+			}
+		} throw new NoMatchingAssessment("No assessments available");
+	}
 
-        return null;
-    }
+	// Submit a completed assessment
+	public void submitAssessment(int token, int studentid, Assessment completed)
+			throws UnauthorizedAccess, NoMatchingAssessment, RemoteException {
+		if (!sessions.isEmpty()) {
+			for (SessionToken session : sessions) {
+				if (session.getToken() == token && session.getStudent().getstudentID() == studentid) {
+					if (assessments.isEmpty()) {
+						for (Assessment assessment : assessments) {
+							if (assessment.getAssociatedID() == studentid
+							 && assessment.getInformation().equals(completed.getInformation())) {
+								assessment = completed;
+							}
+						}
+					} else {
+						throw new NoMatchingAssessment("No assessments available");
+					}
+				} else {
+					throw new UnauthorizedAccess("Session expired");
+				}
+			}
+		} throw new NoMatchingAssessment("No assessments available");
+	}
 
-    // Return an Assessment object associated with a particular course code
-    public Assessment getAssessment(int token, int studentid, String courseCode) throws
-                UnauthorizedAccess, NoMatchingAssessment, RemoteException {
+	public static void main(String[] args) {
+		if (System.getSecurityManager() == null) {
+			System.setSecurityManager(new SecurityManager());
+		}
+		try {
+			String name = "ExamServer";
+			ExamServer engine = new ExamEngine();
+			ExamServer stub = (ExamServer) UnicastRemoteObject.exportObject(engine, 0);
+			Registry registry = LocateRegistry.getRegistry();
+			registry.rebind(name, stub);
 
-        // TBD: You need to implement this method!
-        // For the moment method just returns an empty or null value to allow it to compile
-
-        return null;
-    }
-
-    // Submit a completed assessment
-    public void submitAssessment(int token, int studentid, Assessment completed) throws 
-                UnauthorizedAccess, NoMatchingAssessment, RemoteException {
-
-        // TBD: You need to implement this method!
-    }
-
-    public static void main(String[] args) {
-        if (System.getSecurityManager() == null) {
-            System.setSecurityManager(new SecurityManager());
-        }
-        try {
-            String name = "ExamServer";
-            ExamServer engine = new ExamEngine();
-            ExamServer stub =
-                (ExamServer) UnicastRemoteObject.exportObject(engine, 0);
-            Registry registry = LocateRegistry.getRegistry();
-            registry.rebind(name, stub);
-            System.out.println("ExamEngine bound");
-        } catch (Exception e) {
-            System.err.println("ExamEngine exception:");
-            e.printStackTrace();
-        }
-    }
+			System.err.println("Server ready");
+			System.out.println("ExamEngine bound");
+		} catch (Exception e) {
+			System.err.println("ExamEngine exception:");
+			e.printStackTrace();
+		}
+	}
 }
