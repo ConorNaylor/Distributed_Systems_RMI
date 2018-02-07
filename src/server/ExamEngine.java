26fec2c63dcf9325fe2e0546f9b8defc40867e9f
ConorNaylor <conornaylor@gmail.com>
2018-02-07 22:20:40 +0000

    Fixing merge

--- src/server/ExamEngine.java
+++ src/server/ExamEngine.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 package server;
 
@@ -61,7 +60,7 @@ public class ExamEngine implements ExamServer {
 				throw new NoMatchingAssessment("User has no assessments.");
 			}
 		}
-		return null;
+		throw new UnauthorizedAccess("");
 	}
 
 	// Return an Assessment object associated with a particular course code
@@ -109,145 +108,3 @@ public class ExamEngine implements ExamServer {
 		}
 	}
 }
-=======
-package server;
-
-import java.rmi.RemoteException;
-import java.rmi.registry.LocateRegistry;
-import java.rmi.registry.Registry;
-import java.rmi.server.UnicastRemoteObject;
-import java.util.ArrayList;
-import java.util.List;
-
-import exceptions.NoMatchingAssessment;
-import exceptions.UnauthorizedAccess;
-import interfaces.Assessment;
-import interfaces.ExamServer;
-
-public class ExamEngine implements ExamServer {
-
-	private ArrayList<Student> students;
-	private ArrayList<SessionToken> sessions;
-	private ArrayList<Assessment> assessments;
-
-	// Constructor is required
-	public ExamEngine() throws RemoteException {
-		super();
-		students = new ArrayList<Student>();
-		sessions = new ArrayList<SessionToken>();
-		assessments = new ArrayList<Assessment>();
-		students.add(new Student(14407778, "password"));
-		students.add(new Student(12345678, "password"));
-		students.add(new Student(98765432, "password"));
-		
-	}
-
-	// Implement the methods defined in the ExamServer interface...
-	// Return an access token that allows access to the server for some time period
-	public int login(int studentid, String password) throws UnauthorizedAccess, RemoteException {
-		SessionToken token = null;
-		for (Student student : students) {
-			if (studentid == (student.getstudentID())) {
-				if (password.equals(student.getPassword())) {
-					System.out.println("Logged in");
-					//token = new SessionToken(student);
-					//sessions.add(token);
-
-				} else {
-					throw new UnauthorizedAccess("Incorrect password");
-				}
-			} else {
-				throw new UnauthorizedAccess("Student not registered");
-			}
-		}
-		return 12131;
-	}
-
-	// Return a summary list of Assessments currently available for this studentid
-	public List<String> getAvailableSummary(int token, int studentid)
-			throws UnauthorizedAccess, NoMatchingAssessment, RemoteException {
-		List<String> summary = new ArrayList<String>();
-		for (SessionToken session : sessions) {
-			if (session.getToken() == token && session.getStudent().getstudentID() == studentid) {
-				if (assessments.isEmpty()) {
-					for (Assessment assessment : assessments) {
-						summary.add(assessment.getInformation());
-					}
-				} else {
-					throw new NoMatchingAssessment("No assessments available");
-				}
-			} else {
-				throw new UnauthorizedAccess("Session expired");
-			}
-		}
-		return summary;
-	}
-
-	// Return an Assessment object associated with a particular course code
-	public Assessment getAssessment(int token, int studentid, String courseCode)
-			throws UnauthorizedAccess, NoMatchingAssessment, RemoteException {
-		if (!sessions.isEmpty()) {
-			for (SessionToken session : sessions) {
-				if (session.getToken() == token && session.getStudent().getstudentID() == studentid) {
-					if (assessments.isEmpty()) {
-						for (Assessment assessment : assessments) {
-							if (assessment.getAssociatedID() == studentid
-							// && assessment.getCourseCode().equals(courseCode)
-							) {
-								return assessment;
-							}
-						}
-					} else {
-						throw new NoMatchingAssessment("No assessments available");
-					}
-				} else {
-					throw new UnauthorizedAccess("Session expired");
-				}
-			}
-		}
-		throw new NoMatchingAssessment("No assessments available");
-	}
-
-	// Submit a completed assessment
-	public void submitAssessment(int token, int studentid, Assessment completed)
-			throws UnauthorizedAccess, NoMatchingAssessment, RemoteException {
-		if (!sessions.isEmpty()) {
-			for (SessionToken session : sessions) {
-				if (session.getToken() == token && session.getStudent().getstudentID() == studentid) {
-					if (assessments.isEmpty()) {
-						for (Assessment assessment : assessments) {
-							if (assessment.getAssociatedID() == studentid
-									&& assessment.getInformation().equals(completed.getInformation())) {
-								assessment = completed;
-							}
-						}
-					} else {
-						throw new NoMatchingAssessment("No assessments available");
-					}
-				} else {
-					throw new UnauthorizedAccess("Session expired");
-				}
-			}
-		}
-		throw new NoMatchingAssessment("No assessments available");
-	}
-
-	public static void main(String[] args) {
-		if (System.getSecurityManager() == null) {
-			System.setProperty("java.security.policy", "security.policy");
-			System.setSecurityManager(new SecurityManager());
-		}
-		try {
-			String name = "ExamServer";
-			ExamServer engine = new ExamEngine();
-			ExamServer stub = (ExamServer) UnicastRemoteObject.exportObject(engine, 0);
-			Registry registry = LocateRegistry.getRegistry();
-			registry.rebind(name, stub);
-			System.out.println("ExamEngine bound");
-		} catch (Exception e) {
-			System.err.println("ExamEngine exception:");
-			e.printStackTrace();
-		}
-	}
-}
->>>>>>> 67c7a0f94fe9d03e9bad2cbdcf81d380d27d00eb
--- src/server/MCQAssessment.java
+++ src/server/MCQAssessment.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package server;
 
 import java.util.ArrayList;
@@ -61,64 +60,3 @@ public class MCQAssessment implements Assessment{
 	}
 
 }
-=======
-package server;
-
-import java.util.Date;
-import java.util.List;
-
-import exceptions.InvalidOptionNumber;
-import exceptions.InvalidQuestionNumber;
-import interfaces.Assessment;
-import interfaces.Question;
-
-public class MCQAssessment implements Assessment{
-
-	public MCQAssessment() {
-		
-	}
-	
-	@Override
-	public String getInformation() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Date getClosingDate() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public List<Question> getQuestions() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Question getQuestion(int questionNumber) throws InvalidQuestionNumber {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void selectAnswer(int questionNumber, int optionNumber) throws InvalidQuestionNumber, InvalidOptionNumber {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public int getSelectedAnswer(int questionNumber) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public int getAssociatedID() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-}
->>>>>>> 67c7a0f94fe9d03e9bad2cbdcf81d380d27d00eb
